50efbd6f704d4f1cd309d74be0f30e8baf69f39a
Thomas Ardal <thomasardal@gmail.com>
2021-08-11 08:17:47 +0200

    Debug output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -l
 
+echo $INPUT_LOG_ID
+echo $INPUT_API_KEY
+echo $INPUT_PATH
+echo $INPUT_SOURCEMAP
+echo $INPUT_MINIFIEDJAVASCRIPT
+
 curl --location --request POST "https://api.elmah.io/v3/sourcemaps/$INPUT_LOG_ID?api_key=$INPUT_API_KEY" \
 --header 'Content-Type: multipart/form-data' \
 --form Path=$INPUT_PATH \
