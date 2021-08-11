855766f597be2e696d1d856c32b43b37f59ad00a
Thomas Ardal <thomasardal@gmail.com>
2021-08-11 10:05:37 +0200

    Quotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
 curl --location --request POST https://api.elmah.io/v3/sourcemaps/$INPUT_LOGID?api_key=$INPUT_APIKEY \
--F Path=$INPUT_PATH \
--F SourceMap=@$INPUT_SOURCEMAP;type=application/json \
--F MinifiedJavaScript=@$INPUT_MINIFIEDJAVASCRIPT;type=text/javascript
+-F "Path=$INPUT_PATH" \
+-F "SourceMap=@$INPUT_SOURCEMAP;type=application/json" \
+-F "MinifiedJavaScript=@$INPUT_MINIFIEDJAVASCRIPT;type=text/javascript"
