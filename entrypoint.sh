033013056d4eaeaf6c8850f22ba65682f2234784
Thomas Ardal <thomasardal@gmail.com>
2021-08-11 10:01:33 +0200

    Formatting

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,3 @@
 #!/bin/sh -l
 
-curl -X POST \
-    -F Path=$INPUT_PATH \
-    -F SourceMap=@$INPUT_SOURCEMAP;type=application/json \
-    -F MinifiedJavaScript=@$INPUT_MINIFIEDJAVASCRIPT;type=text/javascript \
-    https://api.elmah.io/v3/sourcemaps/$INPUT_LOGID?api_key=$INPUT_APIKEY
+curl -X POST -F Path=$INPUT_PATH -F SourceMap=@$INPUT_SOURCEMAP;type=application/json -F MinifiedJavaScript=@$INPUT_MINIFIEDJAVASCRIPT;type=text/javascript https://api.elmah.io/v3/sourcemaps/$INPUT_LOGID?api_key=$INPUT_APIKEY
